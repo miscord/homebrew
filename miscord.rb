6cd886a2378fc06d76a7ba571035a730d0250934
Bjornskjald <github@bjorn.ml>
2018-12-13 15:59:49 +0100

    added cask formula

---
+++ miscord.rb
@@ -0,0 +1,10 @@
+cask 'miscord' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://miscord.net/download/macapp.zip'
+  name 'Miscord'
+  homepage 'https://miscord.net/'
+
+  app 'Miscord.app'
+end
